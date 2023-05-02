f69a4ed80da23f93a706065230dffa90c931430b
badriyahaddad <badriya.al.haddad.3915@gmail.com>
2023-05-02 12:37:52 +0200

    last update for today May 2nd

--- lib/widgets/static_widget/profile_card.dart
+++ lib/widgets/static_widget/profile_card.dart
@@ -44,7 +44,7 @@ class _ProfileCardState extends State<ProfileCard> {
           child: CircleAvatar(
             maxRadius: 50,
             backgroundColor: Colors.transparent,
-            child: Image.network(
+            child: Image.asset(
               widget.artWorkImage,
               fit: BoxFit.cover,
               width: size.width / 1,
